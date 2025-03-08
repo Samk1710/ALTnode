7820c4b1abb13085d9170809911b021dd07de491
Samk <sampurnapyne1710@gmail.com>
2025-03-08 15:56:19 +0530

    encryption modules added

---
+++ client/functions/encrypt.ts
@@ -0,0 +1,29 @@
+import * as LitJsSdk from "@lit-protocol/lit-node-client";
+import { encryptString } from "@lit-protocol/encryption";
+import { LIT_NETWORK } from "@lit-protocol/constants";
+import { accessControlConditions } from "../constants/accesControlConditions";
+
+async function encrypt(message: string) {
+    const client = new LitJsSdk.LitNodeClient({
+        alertWhenUnauthorized: false,
+        debug: false,
+        litNetwork: LIT_NETWORK.DatilDev,
+    });
+    await client.connect();
+    // Encrypt the message
+    const { ciphertext, dataToEncryptHash } = await encryptString(
+        {
+            accessControlConditions,
+            dataToEncrypt: message,
+        },
+        client,
+    );
+    await client.disconnect();
+    // Return the ciphertext and dataToEncryptHash
+    return {
+        ciphertext,
+        dataToEncryptHash,
+    };
+}
+
+export default encrypt;
