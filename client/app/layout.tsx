2aeca4f6bd89f0d633ac76bd3a5b9b707051d91b
Prayas <prayaspal04@gmail.com>
2025-04-06 18:38:56 +0530

    refactor

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
-// import Providers from "./provider";
 import Providers from "./_providers/provider";
 
 const inter = Inter({ subsets: ["latin"] });
